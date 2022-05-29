301923cd63e1e0cae4f79a628833ca63addc1a3f
shambles1812 <cjvillafuerte02@gmail.com>
2022-05-30 02:38:49 +0800

    init2

---
+++ app/public/wp-content/themes/twentytwentytwo/functions.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Twenty Twenty-Two functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Two
+ * @since Twenty Twenty-Two 1.0
+ */
+
+
+if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Twenty Twenty-Two 1.0
+	 *
+	 * @return void
+	 */
+	function twentytwentytwo_support() {
+
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'twentytwentytwo_support' );
+
+if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
+
+	/**
+	 * Enqueue styles.
+	 *
+	 * @since Twenty Twenty-Two 1.0
+	 *
+	 * @return void
+	 */
+	function twentytwentytwo_styles() {
+		// Register theme stylesheet.
+		$theme_version = wp_get_theme()->get( 'Version' );
+
+		$version_string = is_string( $theme_version ) ? $theme_version : false;
+		wp_register_style(
+			'twentytwentytwo-style',
+			get_template_directory_uri() . '/style.css',
+			array(),
+			$version_string
+		);
+
+		// Enqueue theme stylesheet.
+		wp_enqueue_style( 'twentytwentytwo-style' );
+
+	}
+
+endif;
+
+add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
+
+// Add block patterns
+require get_template_directory() . '/inc/block-patterns.php';
+
+add_action('save_post','log_when_saved');
+
+function log_when_saved($post_id){
+	$post_log = get_stylesheet_directory(). '/post_log.txt';
+	$message = get_the_title( $post_id) . 'was just saved';
+	$post_url = 'url of the post is ' . get_permalink($post_id);
+	if ( file_exists ( $post_log) ) {
+
+		$file = fopen( $post_log , 'a');
+		fwrite($file,$message."\n" . $post_url);
+	} else {
+		$file = fopen( $post_log , 'w');
+		fwrite($file,$message."\n" . $post_url);
+	}
+	fclose($file);
+}
